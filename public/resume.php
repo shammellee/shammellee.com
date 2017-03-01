c6c25b030daf1295434b7b5d566eeeffb1abff31
Shammel Lee <holler@shammellee.com>
2017-02-28 23:50:58 -0500

    Put Google Analytics and StatCounter code in include files

--- public/resume.php
+++ public/resume.php
@@ -3,34 +3,24 @@ require '../inc/config.php';
 ?>
 
 <!DOCTYPE html>
-	<head>
-		<meta charset="utf-8">
-		<title>Shammel Lee — Resumé</title>
+  <head>
+    <meta charset="utf-8">
+    <title>Shammel Lee — Resumé</title>
     <meta http-equiv="refresh" content="1;url=<?= get_doc("shammel_lee_resume_20170228193234.pdf") ?>">
-		<meta name="description" content="Shammel Lee">
-		<style type="text/css">
-			html,body{background:#30333A;color:#B0B2B4;font:12px Helvetica,Arial,'sans-serif';}
-			a:link,a:visited,a:hover,a:active{color:#B0B2B4;}
-		</style>
-	</head>
-	<body>
-		<h1>Loading&hellip;</h1>
-		<p>Click <a href='<?= get_doc("shammel_lee_resume_20170228193234.pdf") ?>'>here</a> if you are not redirected within 5 seconds.</p>
+    <meta name="description" content="Shammel Lee">
+    <style type="text/css">
+      html,body{background:#30333A;color:#B0B2B4;font:12px Helvetica,Arial,'sans-serif';}
+      a:link,a:visited,a:hover,a:active{color:#B0B2B4;}
+    </style>
+  </head>
+  <body>
+    <h1>Loading&hellip;</h1>
+    <p>Click <a href='<?= get_doc("shammel_lee_resume_20170228193234.pdf") ?>'>here</a> if you are not redirected within 5 seconds.</p>
 
-		<!-- Start of StatCounter Code for Default Guide -->
-		<script type="text/javascript">
-			var sc_project=5026958; 
-			var sc_invisible=1; 
-			var sc_security="505dd106"; 
-		</script>
-		<script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script>
-		<noscript>
-			<div class="statcounter">
-				<a title="web analytics" href="http://statcounter.com/" target="_blank">
-					<img class="statcounter" src="http://c.statcounter.com/5026958/0/505dd106/1/" alt="web analytics">
-				</a>
-			</div>
-		</noscript>
-		<!-- End of StatCounter Code for Default Guide -->
-	</body>
+    <!-- Google Analytics -->
+    <?php require '../inc/google_analytics.html'; ?>
+
+    <!-- StatCounter -->
+    <?php require '../inc/stat_counter.html'; ?>
+  </body>
 </html>
