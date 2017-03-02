20a65f8a65775c14c7763f051710e7820135a449
Shammel Lee <holler@shammellee.com>
2017-03-01 22:51:02 -0500

    Change CloudFront domain to static.shammellee.com

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 
-define('STATIC_ASSET_PATH','http://d298l0sx0jg58a.cloudfront.net');
+define('STATIC_ASSET_PATH','http://static.shammellee.com');
 define('IMAGE_PATH',STATIC_ASSET_PATH . '/img');
 define('CSS_PATH',STATIC_ASSET_PATH . '/css');
 define('JS_PATH',STATIC_ASSET_PATH . '/js');
