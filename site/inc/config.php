98677b32cac18d7153fa196d09f8b142ac1ca4bf
Shammel Lee <holler@shammellee.com>
2017-04-17 19:30:00 -0400

    Add debug mode

--- site/inc/config.php
+++ site/inc/config.php
@@ -1,16 +1,16 @@
 <?php
 error_reporting(0);
 
-define('DS',DIRECTORY_SEPARATOR);
-define('STATIC_ASSET_PATH','http://static.shammellee.com');
-define('CSS_ASSET_VERSION','20170417_183626');
-define('DOC_ASSET_VERSION','20170415_020648');
-define('JS_ASSET_VERSION','20170310_114340');
-define('MEDIA_ASSET_VERSION','20170310_114340');
-define('CSS_PATH',STATIC_ASSET_PATH . '/css/' . CSS_ASSET_VERSION);
-define('DOC_PATH',STATIC_ASSET_PATH . '/docs/' . DOC_ASSET_VERSION);
-define('JS_PATH',STATIC_ASSET_PATH . '/js/' . JS_ASSET_VERSION);
-define('MEDIA_PATH',STATIC_ASSET_PATH . '/media/' . MEDIA_ASSET_VERSION);
+define('DEBUG_MODE',false);
+define('STATIC_ASSET_PATH',DEBUG_MODE ? '' : 'http://static.shammellee.com');
+define('CSS_ASSET_VERSION',DEBUG_MODE ? '' : '/' . '20170417_183626');
+define('DOC_ASSET_VERSION',DEBUG_MODE ? '' : '/' . '20170415_020648');
+define('JS_ASSET_VERSION',DEBUG_MODE ? '' : '/' . '20170310_114340');
+define('MEDIA_ASSET_VERSION',DEBUG_MODE ? '' : '/' . '20170310_114340');
+define('CSS_PATH',STATIC_ASSET_PATH . '/css' . CSS_ASSET_VERSION);
+define('DOC_PATH',STATIC_ASSET_PATH . '/docs' . DOC_ASSET_VERSION);
+define('JS_PATH',STATIC_ASSET_PATH . '/js' . JS_ASSET_VERSION);
+define('MEDIA_PATH',STATIC_ASSET_PATH . '/media' . MEDIA_ASSET_VERSION);
 
 function get_css($css)
 {
